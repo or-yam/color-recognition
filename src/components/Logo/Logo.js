e1890e725d47cd81e402947f977792189db438f8
Oryam <48219965+or-yam@users.noreply.github.com>
2020-04-29 21:00:43 +0300

    -

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,31 +4,12 @@ import './Logo.css';
 import logo from './logo.png';
 const Logo = () => {
   return (
-    <div className="ma4 mt0"   style={{ display: 'flex', justifyContent: 'flex-start', height: "120px", width:"120px" }}>
+    <div className="pa0"   style={{ display: 'flex', justifyContent: 'flex-start', height: "120px", width:"120px" }}>
       <div className="logo br3 shadow-4"  style={{ height: "120px", width:"120px" }}>
         <div className="pa4">
           <img alt="logo" src={logo} />
         </div>
       </div>
-      
-      {/* <Tilt
-        className="Tilt br3-white shadow-3"
-        options={{
-          reverse: false,
-          max: 25,
-          perspective: 150,
-          scale: 1.05,
-          speed: 10,
-          transition: false,
-          axis: null,
-          reset: true,
-        }}
-        style={{ height: 120, width: 120 }}
-      >
-        <div className="Tilt-inner pa4">
-          <img alt="logo" src={logo} />
-        </div>
-      </Tilt> */}
     </div>
   );
 };
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = ({ onRouteChange, isSignedIn, route }) => {
       <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
         <p
           onClick={() => onRouteChange('signout')}
-          className="b ph3 pv2 input-reset ba b--black bg-orange grow pointer f5 dib br4 near-white f1"
+          className="ma2 b ph3 pv2 input-reset ba b--black bg-orange grow pointer f5 dib br4 near-white f1"
         >
           Sign Out
         </p>
