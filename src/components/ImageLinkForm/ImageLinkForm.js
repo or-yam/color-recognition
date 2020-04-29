52f4b5ca07eeddaa41d2a0bcc4473955cae9a445
Oryam <48219965+or-yam@users.noreply.github.com>
2020-04-29 20:44:31 +0300

    small style changes

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,22 +2,22 @@ import React from 'react';
 import './ImageLinkForm.css';
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
-    <div className="">
+    <div>
       <p className="f3 white">
         {'Entre here an Image URL, And it will identify the colors in it'}
       </p>
       <div className="center">
-        <div className="center form pa4 br3 shadow-5">
+        <div className="center form pa4 shadow-5 ba b--transparent br4">
           <input
-            className="f4 pa2 w-80 center"
+            className="f4 pa2 w-80 center ba b--white bb bl bt br3"
             type="tex"
             onChange={onInputChange}
           />
           <button
-            className="w-20 grow f4 link ph3 pv2 dib white bg-green"
+            className="w-20 grow f4 link ph3 pv2 dib white bg-green ba b--green"
             onClick={onButtonSubmit}
           >
-            Submit
+            GO
           </button>
         </div>
       </div>
