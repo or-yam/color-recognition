36d93780ad5b0d19e0f0909cf776423ecff3eb1c
Oryam <48219965+or-yam@users.noreply.github.com>
2021-05-11 22:59:40 +0300

    Refactoring

--- src/components/ColorsList/ColorsList.js
+++ src/components/ColorsList/ColorsList.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import Color from './Color';
-import {
-  calHexByColor,
-  calNameByColor,
-  calAmountByColor,
-} from '../Utilities/Utilities';
+import { calHexByColor, calNameByColor, calAmountByColor } from '../Utilities/Utilities';
 
-const ColorsList = ({ colors }) => {
-  return (
-    <div>
-      {colors.map((color, i) => {
-        return (
-          <Color
-            key={i}
-            demo={colors[i][0]}
-            hex={calHexByColor(colors[i][0])}
-            name={calNameByColor(colors[i][1])}
-            amount={calAmountByColor(colors[i][2])}
-          />
-        );
-      })}
-    </div>
-  );
-};
+const ColorsList = ({ colors }) => (
+  <div>
+    {colors.map(([hex, name, amount], i) => (
+      <Color
+        key={i}
+        demo={hex}
+        hex={calHexByColor(hex)}
+        name={calNameByColor(name)}
+        amount={calAmountByColor(amount)}
+      />
+    ))}
+  </div>
+);
 
 export default ColorsList;
