2d8af601f40135f5faaa2760277da69da5c9018f
Oryam <48219965+or-yam@users.noreply.github.com>
2021-05-11 22:49:54 +0300

    Refactoring

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,41 +1,38 @@
 import React from 'react';
 
-const capitalFirst = (str) => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
 const Rank = ({ name, entries }) => {
+  const capitalFirst = str => str[0].toUpperCase() + str.slice(1);
+
   if (entries === '0') {
     return (
-      <div>
-        <div className="white f3">
-          Welcome
-          <span className="white f2">{` ${capitalFirst(name)}`}</span>
-          {` , You Are Ready To Start`}
-          <p>
-            For inspiration click{' '}
-            <a
-              className="f3 blue"
-              href="https://pixabay.com/images/search/colors/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              here
-            </a>
-          </p>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <div className="white f3">
-          <span className="white f2">{`${capitalFirst(name)}`}</span>
-          {` , You identified colors for:`}
-        </div>
-        <div className="white f2">{`${entries} Images !`}</div>
+      <div className="white f3">
+        Welcome
+        <span className="white f2">{` ${capitalFirst(name)}`}</span>
+        {` , You Are Ready To Start`}
+        <p>
+          For inspiration click{' '}
+          <a
+            className="f3 blue"
+            href="https://pixabay.com/images/search/colors/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
+        </p>
       </div>
     );
   }
+
+  return (
+    <>
+      <div className="white f3">
+        <span className="white f2">{`${capitalFirst(name)}`}</span>
+        {` , You identified colors for:`}
+      </div>
+      <div className="white f2">{`${entries} Images !`}</div>
+    </>
+  );
 };
 
 export default Rank;
