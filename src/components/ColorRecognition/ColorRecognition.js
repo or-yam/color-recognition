a566d7ef3a2794745b03386e918c13321344d9f2
Oryam <48219965+or-yam@users.noreply.github.com>
2021-05-11 23:00:27 +0300

    Refactoring

--- src/components/ColorRecognition/ColorRecognition.js
+++ src/components/ColorRecognition/ColorRecognition.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-const ColorRecognition = ({ imageUrl }) => {
-  return (
-    <div className="center ma mt2">
-      <img id="inputImage" width="500px" height="auto" alt="" src={imageUrl} />
-    </div>
-  );
-};
+const ColorRecognition = ({ imageUrl }) => (
+  <div className="center ma mt2">
+    <img id="inputImage" width="500px" height="auto" alt="color-box" src={imageUrl} />
+  </div>
+);
+
 export default ColorRecognition;
