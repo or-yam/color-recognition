800f658dc7a45bc2692e1a0d6bf9535ec98a8355
Oryam <48219965+or-yam@users.noreply.github.com>
2021-05-11 22:40:43 +0300

    Convert to func component.

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,103 +1,91 @@
 import React from 'react';
 
-class Signin extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      signinEmail: '',
-      signinPassword: '',
-    };
-  }
+const Signin = ({ loadUser, onRouteChange }) => {
+  const [signinEmail, setSigninEmail] = useState('');
+  const [signinPassword, setSigninPassword] = useState('');
 
-  onEmailChange = (event) => {
-    this.setState({ signinEmail: event.target.value });
+  const onEmailChange = event => {
+    setSigninEmail(event.target.value);
   };
 
-  onPasswordChange = (event) => {
-    this.setState({ signinPassword: event.target.value });
+  onPasswordChange = event => {
+    setSigninPassword(event.target.value);
   };
 
-  onSubmitSignin = () => {
+  const onSubmitSignin = () => {
     fetch('https://rocky-savannah-60468.herokuapp.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        email: this.state.signinEmail,
-        password: this.state.signinPassword,
-      }),
+        email: signinEmail,
+        password: signinPassword
+      })
     })
-      .then((response) => response.json())
-      .then((user) => {
+      .then(response => response.json())
+      .then(user => {
         if (user.id) {
-          this.props.loadUser(user);
-          this.props.onRouteChange('home');
+          loadUser(user);
+          onRouteChange('home');
         } else {
           return alert('Check your Email or Password');
         }
       });
   };
 
-  render() {
-    const { onRouteChange } = this.props;
-    return (
-      <div>
-        <article className=" br4 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-          <main className="pa4 black-80">
-            <div className="measure">
-              <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                <legend className="f2 fw6 ph0 mh0 near-white">Sign In</legend>
-                <div className="mt3">
-                  <label
-                    className="db fw6 lh-copy f6 near-white"
-                    htmlFor="email-address"
-                  >
-                    Email
-                  </label>
-                  <input
-                    onChange={this.onEmailChange}
-                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                    type="email"
-                    name="email-address"
-                    id="email-address"
-                  />
-                </div>
-                <div className="mv3">
-                  <label className="db fw6 lh-copy f6 white" htmlFor="password">
-                    Password
-                  </label>
-                  <input
-                    onChange={this.onPasswordChange}
-                    // onKeyPress={this.onSubmitSignin}
-                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                    type="password"
-                    name="password"
-                    id="password"
-                  />
-                </div>
-              </fieldset>
-              <div className="">
+  return (
+    <div>
+      <article className=" br4 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f2 fw6 ph0 mh0 near-white">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6 near-white" htmlFor="email-address">
+                  Email
+                </label>
                 <input
-                  onClick={this.onSubmitSignin}
-                  className="b ph3 pv2 input-reset ba b--black bg-green grow pointer f6 dib br4 near-white f1"
-                  type="submit"
-                  value="Sign in"
+                  onChange={onEmailChange}
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
                 />
               </div>
-              <div className="lh-copy mt3">
-                <p
-                  onClick={() => onRouteChange('register')}
-                  href="#0"
-                  className="b ph3 pv2 input-reset ba b--black bg-blue grow pointer f6 dib br4 near-white f1 "
-                >
-                  Register As New User
-                </p>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6 white" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  onChange={onPasswordChange}
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                />
               </div>
+            </fieldset>
+            <div className="">
+              <input
+                onClick={onSubmitSignin}
+                className="b ph3 pv2 input-reset ba b--black bg-green grow pointer f6 dib br4 near-white f1"
+                type="submit"
+                value="Sign in"
+              />
             </div>
-          </main>
-        </article>
-      </div>
-    );
-  }
-}
+            <div className="lh-copy mt3">
+              <p
+                onClick={() => onRouteChange('register')}
+                href="#0"
+                className="b ph3 pv2 input-reset ba b--black bg-blue grow pointer f6 dib br4 near-white f1 "
+              >
+                Register As New User
+              </p>
+            </div>
+          </div>
+        </main>
+      </article>
+    </div>
+  );
+};
 
 export default Signin;
