767540775df1d698e303fd7f7bd179bed40e2538
Oryam <48219965+or-yam@users.noreply.github.com>
2021-05-11 22:36:00 +0300

    Convert to func component.

--- src/App.js
+++ src/App.js
@@ -1,154 +1,216 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import ColorRecognition from './components/ColorRecognition/ColorRecognition';
-import ColorsList from './components/ColorsList/ColorsList';
-import Signin from './components/Signin/Signin';
-import Register from './components/Register/Register';
+import React, { useState } from 'react';
 
-const initialState = {
-  input: '',
-  imageUrl: '',
-  colors: [],
-  route: 'signin',
-  isSignedIn: false,
-  user: {
+export default function AppFunc() {
+  const [user, setUser] = useState({
     id: '0',
     name: '',
     email: '',
     entries: 0,
-    joined: '',
-  },
-};
+    joined: ''
+  });
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [colors, setColors] = useState([]);
+  const [isSignin, setIsSignin] = useState(false);
+  const [route, setRoute] = useState('signin');
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = initialState;
-  }
+  const onRouteChange = route => {
+    if (route === 'signout') {
+      setRoute('signin');
+    }
+    if (route === 'home') {
+      setIsSignin(true);
+    }
+    setRoute(route);
+  };
 
-  loadUser = (data) => {
-    this.setState({
-      user: {
-        id: data.id,
-        name: data.name,
-        email: data.email,
-        entries: data.entries,
-        joined: data.joined,
-      },
+  const loadUser = ({ id, name, email, entries, joined }) => {
+    setUser({
+      id,
+      name,
+      email,
+      entries,
+      joined
     });
   };
 
-  calculateColors = (data) => {
-    const clarifaiColors = data.outputs[0].data.colors;
-    let colorList = [];
-    for (let i = 0; i < clarifaiColors.length; i++) {
-      let color = [
-        clarifaiColors[i].w3c.hex,
-        clarifaiColors[i].w3c.name,
-        clarifaiColors[i].value,
-      ];
-      colorList.push(color);
-    }
-    return colorList;
+  const calculateColors = ({ outputs }) => outputs[0].data.colors.map(({ w3c, value }) => [w3c.hex, w3c.name, value]);
+
+  const displayColors = colors => {
+    setColors(colors);
   };
 
-  displayColors = (colors) => {
-    this.setState({ colors: colors });
-  };
-
-  onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
+  const onButtonSubmit = () => {
+    setImageUrl(input);
     fetch('https://rocky-savannah-60468.herokuapp.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        input: this.state.input,
-      }),
+        input
+      })
     })
-      .then((response) => response.json())
-      .then((response) => {
+      .then(response => response.json())
+      .then(response => {
         if (response) {
           fetch('https://rocky-savannah-60468.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-              id: this.state.user.id,
-            }),
+              id: user.id
+            })
           })
-            .then((response) => response.json())
-            .then((count) => {
-              this.setState(Object.assign(this.state.user, { entries: count }));
+            .then(response => response.json())
+            .then(count => {
+              setUser({ ...user, entries: count });
             })
             .catch(console.log);
         }
 
-        this.displayColors(this.calculateColors(response));
+        displayColors(calculateColors(response));
       })
-      .catch((err) => console.log(err));
+      .catch(err => console.log(err));
   };
 
-  onInputChange = (event) => {
-    this.setState({ input: event.target.value });
+  const onInputChange = event => {
+    setInput(event.target.value);
   };
 
-  onRouteChange = (route) => {
-    if (route === 'signout') {
-      this.setState(initialState);
-    } else if (route === 'home') {
-      this.setState({ isSignedIn: true });
-    }
-    this.setState({ route: route });
-  };
-
-  render() {
-    const { isSignedIn, route, imageUrl, colors } = this.state;
-    return (
-      <div className="App">
-        <div className="topBar ma4">
-          <Logo />
-          <Navigation
-            isSignedIn={isSignedIn}
-            onRouteChange={this.onRouteChange}
-            route={route}
-          />
-        </div>
-        {route === 'home' ? (
-          <div>
-            <Rank
-              name={this.state.user.name}
-              entries={this.state.user.entries}
-            />
-            <ImageLinkForm
-              onInputChange={this.onInputChange}
-              onButtonSubmit={this.onButtonSubmit}
-            />
-            <div>
-              <ColorRecognition imageUrl={imageUrl} />
-              <ColorsList colors={colors} />
-            </div>
-          </div>
-        ) : route === 'signin' ? (
-          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-        ) : (
-          <Register
-            loadUser={this.loadUser}
-            onRouteChange={this.onRouteChange}
-          />
-        )}
-        <footer className="f5 white">
-          Made with{' '}
-          <a className="blue" href="https://www.clarifai.com/">
-            Clarifai
-          </a>{' '}
-          api
-        </footer>
+  return (
+    <div className="App">
+      <div className="topBar ma4">
+        <Logo />
+        <Navigation isSignin={isSignin} onRouteChange={onRouteChange} route={route} />
       </div>
-    );
-  }
+      {route === 'home' && (
+        <div>
+          <Rank name={user.name} entries={user.entries} />
+          <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} />
+          <div>
+            <ColorRecognition imageUrl={imageUrl} />
+            <ColorsList colors={colors} />
+          </div>
+        </div>
+      )}
+      {route === 'signin' ? (
+        <Signin loadUser={loadUser} onRouteChange={onRouteChange} />
+      ) : (
+        <Register loadUser={loadUser} onRouteChange={onRouteChange} />
+      )}
+      <footer className="f5 white">
+        Made with
+        <a className="blue" href="https://www.clarifai.com/">
+          Clarifai
+        </a>
+        api
+      </footer>
+    </div>
+  );
 }
+import React, { useState } from 'react';
 
-export default App;
+export default function AppFunc() {
+  const [user, setUser] = useState({
+    id: '0',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  });
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [colors, setColors] = useState([]);
+  const [isSignin, setIsSignin] = useState(false);
+  const [route, setRoute] = useState('signin');
+
+  const onRouteChange = route => {
+    if (route === 'signout') {
+      setRoute('signin');
+    }
+    if (route === 'home') {
+      setIsSignin(true);
+    }
+    setRoute(route);
+  };
+
+  const loadUser = ({ id, name, email, entries, joined }) => {
+    setUser({
+      id,
+      name,
+      email,
+      entries,
+      joined
+    });
+  };
+
+  const calculateColors = ({ outputs }) => outputs[0].data.colors.map(({ w3c, value }) => [w3c.hex, w3c.name, value]);
+
+  const displayColors = colors => {
+    setColors(colors);
+  };
+
+  const onButtonSubmit = () => {
+    setImageUrl(input);
+    fetch('https://rocky-savannah-60468.herokuapp.com/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input
+      })
+    })
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
+          fetch('https://rocky-savannah-60468.herokuapp.com/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              setUser({ ...user, entries: count });
+            })
+            .catch(console.log);
+        }
+
+        displayColors(calculateColors(response));
+      })
+      .catch(err => console.log(err));
+  };
+
+  const onInputChange = event => {
+    setInput(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <div className="topBar ma4">
+        <Logo />
+        <Navigation isSignin={isSignin} onRouteChange={onRouteChange} route={route} />
+      </div>
+      {route === 'home' && (
+        <div>
+          <Rank name={user.name} entries={user.entries} />
+          <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} />
+          <div>
+            <ColorRecognition imageUrl={imageUrl} />
+            <ColorsList colors={colors} />
+          </div>
+        </div>
+      )}
+      {route === 'signin' ? (
+        <Signin loadUser={loadUser} onRouteChange={onRouteChange} />
+      ) : (
+        <Register loadUser={loadUser} onRouteChange={onRouteChange} />
+      )}
+      <footer className="f5 white">
+        Made with
+        <a className="blue" href="https://www.clarifai.com/">
+          Clarifai
+        </a>
+        api
+      </footer>
+    </div>
+  );
+}
